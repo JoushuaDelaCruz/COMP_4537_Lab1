fe8d0e57776fb10565d016b6492ed7001824387f
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-08 16:50:49 -0700

    Separated Store logics

--- js/search.js
+++ js/search.js
@@ -29,38 +29,6 @@ const _getWordSuccessHandler = (response) => {
   <h2> ${response.definition || "No definition yet"} </h2>`;
 };
 
-const _postReq = (params, errHandler, successHandler) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", URL + params, true);
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4 && xhr.status === 201) {
-      const response = JSON.parse(xhr.responseText);
-      successHandler(response);
-    }
-    if (xhr.readyState === 4 && xhr.status !== 201) {
-      const response = JSON.parse(xhr.responseText);
-      errHandler(response);
-    }
-  };
-  xhr.send();
-};
-
-const _defineWordSuccessHandler = (response) => {
-  console.log(response);
-  const displayResponse = document.getElementsByTagName("main")[0];
-  displayResponse.innerHTML = `
-  <h1> Word: ${response.word} </h1> | 
-  <h1> Definition: ${response.definition} </h1> | 
-  <h1> Total Entries: ${response.totalEntries} </h1>`;
-};
-
-const defineWord = () => {
-  const word = document.getElementById("addWord").value;
-  const description = document.getElementById("addDefinition").value;
-  const params = `?word=${word}&definition=${description}`;
-  _postReq(params, _errHandler, _defineWordSuccessHandler);
-};
-
 const getWord = () => {
   const word = document.getElementById("searchWord").value;
   const params = `?word=${word}`;
